bf50f63e9f6475179bc820b7d296bd8910075e9e
agruzman <agruzman@yrevah1-MOBL3.ger.corp.intel.com>
2016-07-27 00:02:09 +0300

    ExecSpec - a base class for any exec action

---
+++ ExecSpec/exec_given_config.py
@@ -0,0 +1,17 @@
+'''
+Created on Jul 27, 2016
+
+@author: agruzman
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
