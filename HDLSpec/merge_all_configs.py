b475144a09259e19cc6f12a95a0d235bad5f0e26
agruzman <agruzman@yrevah1-MOBL3.ger.corp.intel.com>
2016-07-27 00:02:25 +0300

    merging all configs

---
+++ HDLSpec/merge_all_configs.py
@@ -0,0 +1,17 @@
+'''
+Created on Jul 26, 2016
+
+@author: agruzman
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
