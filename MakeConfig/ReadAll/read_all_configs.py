81a306be4f2171f1a3e94e1ef2072d772a61efb2
agruzman <agruzman@yrevah1-MOBL3.ger.corp.intel.com>
2016-07-29 16:20:34 +0300

    Fixinf the name of the class

---
+++ MakeConfig/ReadAll/read_all_configs.py
@@ -0,0 +1,17 @@
+'''
+Created on Jul 29, 2016
+
+@author: agruzman
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
